3d2c8e72bfca87bf6aec153480161895f9eeec42
Yuri Azevedo <yuriazevedo11@gmail.com>
2020-05-02 02:01:42 -0300

    feat: 🎸 Add base styling of login screen

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
 
-import { Container } from './styles';
+import {
+  Container,
+  ImageContainer,
+  LoginImage,
+  ButtonArea,
+  SignInButton,
+  ButtonText,
+} from './styles';
 
-const Login: React.FC = () => <Container />;
+const Login: React.FC = () => (
+  <Container>
+    <ImageContainer>
+      <LoginImage />
+    </ImageContainer>
+
+    <ButtonArea>
+      <SignInButton onPress={() => {}}>
+        <ButtonText>SIGN IN</ButtonText>
+      </SignInButton>
+      <SignInButton onPress={() => {}} color="#2e71dc">
+        <ButtonText color="#fff">SIGN IN WITH FACEBOOK</ButtonText>
+      </SignInButton>
+    </ButtonArea>
+  </Container>
+);
 
 export default Login;
--- src/styles.ts
+++ src/styles.ts
@@ -1,3 +1,50 @@
+import { Dimensions } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 
-export const Container = styled.View``;
+const { height } = Dimensions.get('window');
+
+export const Container = styled.View`
+  flex: 1;
+  background: #fff;
+  justify-content: flex-end;
+`;
+
+export const ImageContainer = styled.View`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+`;
+
+export const LoginImage = styled.Image.attrs({
+  source: require('../assets/login-background.jpg'),
+})`
+  flex: 1;
+  height: null;
+  width: null;
+`;
+
+export const ButtonArea = styled.View`
+  height: ${height / 3}px;
+`;
+
+interface ButtonProps {
+  color?: string;
+}
+
+export const SignInButton = styled(RectButton)<ButtonProps>`
+  background: ${({ color }) => color || '#fff'};
+  height: 70px;
+  margin: 5px 20px;
+  border-radius: 35px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ButtonText = styled.Text<ButtonProps>`
+  font-size: 20px;
+  font-weight: bold;
+  color: ${({ color }) => color || '#333'};
+`;
